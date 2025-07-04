5f0f6c7c53d69dd0dc4d95275b0e1658b0cc1c7b
Pedro Henrique <karpatech2025@gmail.com>
2025-07-04 10:06:34 -0300

    queue.ts

---
+++ Desktop/typeScript/collection/pilha/stack.ts
@@ -0,0 +1,10 @@
+import { Stack } from "../stackMetodos";
+
+const pilha = new Stack<string>();
+
+pilha.push("Prato Branco");
+pilha.push("Prato Azul");
+pilha.push("Prato Amarelo");
+pilha.push("Prato Verde");
+pilha.push("Prato Preto");
+
---
+++ Desktop/typeScript/collection/queue.ts
@@ -0,0 +1,43 @@
+import ler = require("readline-sync");
+
+import { Queue } from "./queueMetodos";
+import { Stack } from "./stackMetodos";
+
+const fila = new Queue<string>();
+
+let continua: string = "";
+
+do {
+  console.log("####### Fila de atendimento ###############");
+  console.log("### Digite 1 cadastrar usuario ############");
+  console.log("### Digite 2 finalizar atendimento ########");
+  console.log("### Digite 3 ver lista de usuario #########");
+  console.log("### Digite 4 próximo da fila #########");
+
+  let opcao = ler.questionInt(" -> ");
+
+  switch (opcao) {
+    case 1:
+      fila.enqueue(ler.question("Nome: "));
+      break;
+
+    case 2:
+      fila.dequeue();
+
+      break;
+
+    case 3:
+      fila.printQueue();
+
+      break;
+
+    case 4:
+      console.log(fila.peek());
+
+      break;
+    default:
+      console.log("Opção inválida!");
+  }
+
+  continua = ler.question("Diga 's' para continuar: ").toLowerCase();
+} while (continua === "s");
